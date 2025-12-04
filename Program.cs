976693db522a14a9bf4c819f1d1067e35b4b5a78
artjom-spessivtsev <artjom.spesivcev@gmail.com>
2025-12-04 09:57:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ForLoopHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //teha konsoolirakendus, kus kasutad for loopi
+            //kolm korda näidatakse Tere tulemast
+            ushort i = 0;
+
+            for (; ; )
+            {
+                Console.WriteLine("Tere inimene");
+                i++;
+                if (i > 2)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
